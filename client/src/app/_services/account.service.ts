ccfce95c50a2b53e7ca6fa7a641a45b30bcc8814
shantosiddiq <shanto.siddiq1@gmail.com>
2021-06-28 21:12:59 +0600

    Login and Register implemented clientside

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1); //how many previous user gonna store. Storing the current User
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http:HttpClient) { }
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user?: User) {
+    if(user == null)
+    {
+      this.currentUserSource.next();
+    }
+    else{
+      this.currentUserSource.next(user);
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next();
+  }
+}
